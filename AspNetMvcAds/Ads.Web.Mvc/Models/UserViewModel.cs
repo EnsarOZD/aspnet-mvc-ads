b83ee970960c5eadd48f5db6c6f676fe6bff41f9
Betul Topkan <betultopkann@gmail.com>
2023-10-08 23:07:47 +0300

    index edit

--- AspNetMvcAds/Ads.Web.Mvc/Models/UserViewModel.cs
+++ AspNetMvcAds/Ads.Web.Mvc/Models/UserViewModel.cs
@@ -2,8 +2,13 @@
 {
 	public class UserViewModel
 	{
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public string FirstName { get; set; } = string.Empty;
+
+		public string LastName { get; set; } = string.Empty;
+
+		public string Email { get; set; } = string.Empty;
+	}
 }
