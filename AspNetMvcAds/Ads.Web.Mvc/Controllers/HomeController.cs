f0e9bac37a964ac7db92db486170ad3171c31e15
furkanucgull <furkanucgull@gmail.com>
2023-10-11 19:19:29 +0300

    Revert "Merge pull request #28 from EnsarOZD/feat-22-task6"
    This reverts commit f7355048fc4b58703a55929b23282769d32e8abf, reversing
    changes made to 818bb808e6bfcb178bccaadb516d6d92cd7d579a.

--- AspNetMvcAds/Ads.Web.Mvc/Controllers/HomeController.cs
+++ AspNetMvcAds/Ads.Web.Mvc/Controllers/HomeController.cs
@@ -1,27 +1,41 @@
-﻿using Ads.Web.Mvc.Models;
+﻿using Ads.Data;
+using Ads.Web.Mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Ads.Web.Mvc.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger, AppDbContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			var categoryList = _context.CategoryEntities.Select(x => new CategoryViewModel
+			{
+				Id = x.Id,
+				Name = x.Name,
+			}).ToList();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			var model = new HomeViewModel
+			{
+				Categories = categoryList,
+			};
+
+			return View(model);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
--- AspNetMvcAds/Ads.Web.Mvc/Models/UserViewModel.cs
+++ AspNetMvcAds/Ads.Web.Mvc/Models/UserViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Ads.Web.Mvc.Models
+namespace Ads.Web.Mvc.Models
 {
+
     public class UserViewModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
         public string FirstName { get; set; } = string.Empty;
 
         public string LastName { get; set; } = string.Empty;
@@ -10,4 +14,5 @@
 
         public string Phone { get; set; } = string.Empty;
     }
+
 }
