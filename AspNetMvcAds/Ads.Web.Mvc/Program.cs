757a8d96b9e0547dddc5c35dbca3360f0616a57f
Almıla <almilaoztaskin@gmail.com>
2023-10-18 11:30:34 +0300

    Added AdminLayout

--- AspNetMvcAds/Ads.Web.Mvc/Program.cs
+++ AspNetMvcAds/Ads.Web.Mvc/Program.cs
@@ -61,11 +61,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "areas",
+	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
