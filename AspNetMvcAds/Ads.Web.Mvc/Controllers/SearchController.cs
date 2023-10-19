3303c1a735eacae6adafa29c27d660504bfa619e
furkanucgull <furkanucgull@gmail.com>
2023-10-19 17:57:07 +0300

    titlesearch

---
+++ AspNetMvcAds/Ads.Web.Mvc/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using Ads.Data;
+using Ads.Web.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ads.Web.Mvc.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SearchController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(string searchContent)
+        {
+            var filteredTitle = from s in _context.AdvertEntities
+                                select s;
+            if (!string.IsNullOrEmpty(searchContent))
+            {
+                filteredTitle = filteredTitle.Where(p => p.Title.Contains(searchContent));
+
+            }
+            var filteredPosts = from s in _context.CategoryEntities
+                                select s;
+            var titles = filteredTitle.ToList();
+            var categories= filteredPosts.ToList();
+            var viewModel = new SearchViewModel
+            {
+                Titles = titles,
+                //Categories = categories,
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ AspNetMvcAds/Ads.Web.Mvc/Models/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Ads.Data.Entities;
+
+namespace Ads.Web.Mvc.Models
+{
+    public class SearchViewModel
+    {
+        public List<AdvertEntity> Titles { get; set; }
+        public List<CategoryEntity> Categories { get; set; }
+        //public List<CategoryViewModel> Categories { get; set; }
+    }
+}
