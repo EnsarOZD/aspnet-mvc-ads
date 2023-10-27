accc2de19a73130ac8d63a924b934edc22fe1a6e
furkanucgull <furkanucgull@gmail.com>
2023-10-27 22:59:32 +0300

    Edit

--- AspNetMvcAds/Ads.Web.Mvc/Areas/Admin/Models/UserViewModel.cs
+++ AspNetMvcAds/Ads.Web.Mvc/Areas/Admin/Models/UserViewModel.cs
@@ -1,19 +1,14 @@
 ﻿namespace Ads.Web.Mvc.Areas.Admin.Models
 {
-	public class UserViewModel
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public DateTimeOffset CreatedAt { get; set; }
-		public string FirstName { get; set; } = string.Empty;
-
-		public string LastName { get; set; } = string.Empty;
-
-		public string Email { get; set; } = string.Empty;
-
-		public string Phone { get; set; } = string.Empty;
-		public string Address {  get; set; } = string.Empty;
-		public bool IsEmailConfirmed { get; set; }
-		public string Roles { get; set; } = string.Empty;
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public bool IsEmailConfirmed { get; set; }
+        public string Roles { get; set; } = string.Empty;
     }
 }
