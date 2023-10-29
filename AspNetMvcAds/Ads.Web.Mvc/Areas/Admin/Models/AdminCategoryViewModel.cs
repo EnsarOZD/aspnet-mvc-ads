ff4a98f6c50b076a7fe9c9303c74fd8a5fec0533
BetulTopkan <131692701+BetulTopkan@users.noreply.github.com>
2023-10-29 14:47:45 +0300

    added category view

---
+++ AspNetMvcAds/Ads.Web.Mvc/Areas/Admin/Models/AdminCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ads.Web.Mvc.Areas.Admin.Models
+{
+	public class AdminCategoryViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTimeOffset CreatedAt { get; set; }
+		public DateTimeOffset UpdatedAt { get; set; }
+	}
+}
